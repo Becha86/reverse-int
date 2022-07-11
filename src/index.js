cb9a530657da2626058a15d24967fe080f2d1837
Becha86 <101560811+Becha86@users.noreply.github.com>
2022-07-12 00:36:35 +0600

    init: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let numToString = num.toString();
+  let arr = numToString.split('');
+  return Number(arr.reverse().join(''));
 }
